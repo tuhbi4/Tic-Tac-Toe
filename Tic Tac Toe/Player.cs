dc1459d605372c0bdb8d18145bf02b437bbf06ce
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-09-21 03:21:43 +0300

    Add Player and Point classes

--- Tic Tac Toe/Player.cs
+++ Tic Tac Toe/Player.cs
@@ -1,22 +1,49 @@
-﻿namespace TicTacToe
+﻿using System;
+
+namespace TicTacToe
 {
     public class Player
     {
         public string Name { get; }
-        public char Filler { get; }
+        public char Symbol { get; }
         public int CountOfCombinationMade { get; private set; }
-        public bool GameOver { get; set; }
+        public bool Winner { get; set; }
 
         public Player(string name, char filler)
         {
             Name = name;
-            Filler = filler;
+            Symbol = filler;
             CountOfCombinationMade = 0;
         }
 
         public void IncrementCountOfCombinationMade()
         {
             CountOfCombinationMade++;
+        }
+    }
+
+    public class Bot : Player
+    {
+        public int FirstDice { get; private set; }
+        public int SecondDice { get; private set; }
+        private static readonly Random diceRandomValue = new();
+
+        public Bot(string name, char symbol) : base(name, symbol)
+        {
+            MakeTurn();
+        }
+
+        private void MakeTurn()
+        {
+            FirstDice = diceRandomValue.Next(1, 6);
+            SecondDice = diceRandomValue.Next(1, 6);
+            DrawTheDices();
+        }
+        public void DrawTheDices()
+        {
+            Console.WriteLine("Throwing the dices ...\n+-------+       +-------+\n|\\       \\     /       /|");
+            Console.WriteLine($"| +-------+   +-------+ |\n| |       |   |       | | \n+ |   {FirstDice}   |   |   {SecondDice}   | +");
+            Console.WriteLine(" \\|       |   |       |/ \n  +-------+   +-------+  ");
         }
     }
 }
---
+++ Tic Tac Toe/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int coordinateX, int coordinateY)
+        {
+            X = coordinateX;
+            Y = coordinateY;
+        }
+    }
+
+    public class RandomPoint : Point
+    {
+        private static readonly Random randomCoordinate = new();
+
+        public RandomPoint(int startOn, int xEndOn, int yEndOn) : base(randomCoordinate.Next(startOn, xEndOn), randomCoordinate.Next(startOn, yEndOn))
+        {
+        }
+    }
+}
