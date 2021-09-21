b7501220bf7b7d9499b2f4e03ab9f8457c87ca06
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-09-21 14:59:56 +0300

    Documentation + Refactoring

--- TicTacToe/Validator.cs
+++ TicTacToe/Validator.cs
@@ -4,6 +4,11 @@ namespace TicTacToe
 {
     public static class Validator
     {
+        /// <summary>
+        /// Checks that the user entered a number, not a string, and converts the string representation of the number to its 32-bit signed integer equivalent.
+        /// </summary>
+        /// <param name="number">When this method returns, contains the 32-bit signed integer value equivalent of the number from user input.</param>
+        /// <returns>32-bit signed integer equivalent to user input.</returns>
         public static int NumberValidation()
         {
             if (!int.TryParse(Console.ReadLine(), out int number))
@@ -12,41 +17,21 @@ namespace TicTacToe
                 return NumberValidation();
             }
             return number;
-        }
+        }        
 
-        public static int SizeValidation(string valueName, int maxValue)
+        /// <summary>
+        /// Checks that the user has entered a valid number of <see cref="valueName"/> and converts the string representation of the number to its 32-bit signed integer equivalent.
+        /// </summary>
+        /// <param name="valueName">Name of the variable, the value of which needs to be checked.</param>
+        /// <param name="minValue">Minimum allowable value for the variable to be checked.</param>
+        /// <param name="maxValue">Maximum allowable value for the variable to be checked..</param>
+        /// <returns>32-bit signed integer equivalent to user input.</returns>
+        public static int ValueOfVariableValidation(string valueName, int minValue, int maxValue)
         {
-            
-            Console.WriteLine($"Set the odd number of board {valueName}: (minimum is: 3; maximum is {maxValue})");
+            Console.WriteLine($"Set the {valueName}: (minimum is: {minValue}; maximum is {maxValue})");
             while (true)
             {
-                var size = NumberValidation();
-                if (size < 3)
-                {
-                    Console.WriteLine($"Please enter a value greater than or equal to 3");
-                }
-                else if (size > maxValue)
-                {
-                    Console.WriteLine($"Please enter a value less than or equal to {maxValue}");
-                }
-                else if (size % 2 == 0)
-                {
-                    Console.WriteLine($"Please enter a odd value");
-                }
-                else
-                {
-                    return size;
-                }
-            }
-        }
-
-        public static int ValueValidation(string valueName, int minValue, int maxValue)
-        {
-            int valueOut;
-            Console.WriteLine($"Set the number of {valueName}: (minimum is: {minValue}; maximum is {maxValue})");
-            while (true)
-            {
-                valueOut = NumberValidation();
+                var valueOut = NumberValidation();
                 if (valueOut < minValue)
                 {
                     Console.WriteLine($"Please enter a value greater than or equal to {minValue}");
@@ -54,6 +39,39 @@ namespace TicTacToe
                 else if (valueOut > maxValue)
                 {
                     Console.WriteLine($"Please enter a value less than or equal to {maxValue}");
+                }
+                else
+                {
+                    return valueOut;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks that the user has entered a valid number of <see cref="valueName"/> and converts the string representation of the number to its 32-bit signed integer equivalent.
+        /// </summary>
+        /// <param name="valueName">Name of the variable, the value of which needs to be checked.</param>
+        /// <param name="minValue">Minimum allowable value for the variable to be checked.</param>
+        /// <param name="maxValue">Maximum allowable value for the variable to be checked..</param>
+        /// <returns>32-bit signed integer equivalent to user input.</returns>
+        public static int ValueAndOddOfVariableValidation(string valueName, int minValue, int maxValue)
+        {
+
+            Console.WriteLine($"Set the {valueName}: (minimum is: {minValue}; maximum is {maxValue})");
+            while (true)
+            {
+                var valueOut = NumberValidation();
+                if (valueOut < minValue)
+                {
+                    Console.WriteLine($"Please enter a value greater than or equal to {minValue}");
+                }
+                else if (valueOut > maxValue)
+                {
+                    Console.WriteLine($"Please enter a value less than or equal to {maxValue}");
+                }
+                else if (valueOut % 2 == 0)
+                {
+                    Console.WriteLine($"Please enter an odd value");
                 }
                 else
                 {
