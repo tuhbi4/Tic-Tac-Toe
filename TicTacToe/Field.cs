7e7d254d24236ba3569ae5672ef17e62e4fce1ce
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-09-21 13:58:07 +0300

    Class renamed

--- TicTacToe/Field.cs
+++ TicTacToe/Field.cs
@@ -2,23 +2,23 @@
 
 namespace TicTacToe
 {
-    public class Point
+    public class Field
     {
         public int X { get; }
         public int Y { get; }
 
-        public Point(int coordinateX, int coordinateY)
+        public Field(int coordinateX, int coordinateY)
         {
             X = coordinateX;
             Y = coordinateY;
         }
     }
 
-    public class RandomPoint : Point
+    public class RandomField : Field
     {
         private static readonly Random randomCoordinate = new();
 
-        public RandomPoint(int startOn, int xEndOn, int yEndOn) : base(randomCoordinate.Next(startOn, xEndOn), randomCoordinate.Next(startOn, yEndOn))
+        public RandomField(int startOn, int xEndOn, int yEndOn) : base(randomCoordinate.Next(startOn, xEndOn), randomCoordinate.Next(startOn, yEndOn))
         {
         }
     }
