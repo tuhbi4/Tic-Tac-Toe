219037055e69fe1f9895b7af9d7a5b07bfd0466b
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-09-23 22:25:28 +0300

    Add documentation + incapsulation + future improvement

--- TicTacToe/Player.cs
+++ TicTacToe/Player.cs
@@ -2,6 +2,13 @@
 
 namespace TicTacToe
 {
+    /// <summary>
+    /// Represents a player with the following properties:
+    /// string type properties <see cref="Name"/> and <see cref="Symbol"/>;
+    /// 32-bit integer <see cref="CountOfCombinationsMade"/>, that always greater than or equal to zero;
+    /// boolean <see cref="Winner"/>.
+    /// Provides a method to increment count of combinations maded.
+    /// </summary>
     public class Player
     {
         public string Name { get; }
@@ -9,34 +16,58 @@ namespace TicTacToe
         public int CountOfCombinationsMade { get; private set; }
         public bool Winner { get; set; }
 
-        public Player(string name, string filler)
+        public Player() : this(string.Empty, string.Empty)
+        {
+        }
+
+        public Player(string name, string symbol)
         {
             Name = name;
-            Symbol = filler;
+            Symbol = symbol;
             CountOfCombinationsMade = 0;
         }
 
-        public void IncrementCountOfCombinationMade()
+        /// <summary>
+        /// Increases the count of maded combinations.
+        /// </summary>
+        /// <param name="count">The number by which to increase the counter.</param>
+        public void IncreaseCountOfCombinationMade(int count)
         {
-            CountOfCombinationsMade++;
+            CountOfCombinationsMade += count;
         }
     }
 
+    /// <summary>
+    /// Represents a simple bot with the following properties:
+    /// 32-bit integer <see cref="FirstDice"/> and <see cref="SecondDice"/> that are represents a random value in a given range.
+    /// Provides method to generate a new values.
+    /// </summary>
     public class Bot : Player
     {
         public int FirstDice { get; private set; }
         public int SecondDice { get; private set; }
         private static readonly Random diceRandomValue = new();
 
+        public Bot() : base (string.Empty, string.Empty)
+        {
+        }
+
         public Bot(string name, string symbol) : base(name, symbol)
         {
         }
 
-        public void MakeTurn(int maxValue, out int coordinateX, out int coordinateY)
+        /// <summary>
+        /// Generates new values.
+        /// </summary>
+        /// <param name="minValue">The minimum number in the range.</param>
+        /// <param name="maxValue">The maximum number in the range.</param>
+        /// <param name="coordinateX">Output value for column number .</param>
+        /// <param name="coordinateY">Output value for row number.</param>
+        public void MakeTurn(int minValue, int maxValue, out int coordinateX, out int coordinateY)
         {
-            FirstDice = diceRandomValue.Next(1, maxValue);
+            FirstDice = diceRandomValue.Next(minValue, maxValue);
             coordinateX = FirstDice;
-            SecondDice = diceRandomValue.Next(1, maxValue);
+            SecondDice = diceRandomValue.Next(minValue, maxValue);
             coordinateY = SecondDice;
             DrawTheDices();
         }
@@ -45,6 +76,32 @@ namespace TicTacToe
             Console.WriteLine("Computer roll the dices ...\n+-------+       +-------+\n|\\       \\     /       /|");
             Console.WriteLine($"| +-------+   +-------+ |\n| |       |   |       | | \n+ |   {FirstDice}   |   |   {SecondDice}   | +");
             Console.WriteLine(" \\|       |   |       |/ \n  +-------+   +-------+  ");
+        }
+    }
+
+    /// <summary>
+    /// Represents a simple bot with the following properties:
+    /// 32-bit integer <see cref="FirstDice"/> and <see cref="SecondDice"/> that are represents a random value in a given range.
+    /// Provides method to generate a new values.
+    /// </summary>
+    public class BotAI : Player
+    {
+        public BotAI() : base(string.Empty, string.Empty)
+        {
+        }
+
+        public BotAI(string name, string symbol) : base(name, symbol)
+        {
+        }
+
+        /// <summary>
+        /// Generates new values. Uses an intelligent generation algorithm.
+        /// </summary>
+        /// <param name="coordinateX">Output value for column number .</param>
+        /// <param name="coordinateY">Output value for row number.</param>
+        public void MakeIntellectualTurn()
+        {
+            throw new NotSupportedException(); // TODO: Will be implemented in the future
         }
     }
 }
